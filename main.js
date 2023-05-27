9d8cddfac368dea49dab237e9a5f0fee886f8d3e
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2023-05-27 22:48:51 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,131 @@
+function main()
+{
+	const iters = 256;
+	let canvas = document.getElementById("c");
+	let mx = 0;
+	let my = 0;
+	let md = 0;
+	let mr = 0;
+	canvas.addEventListener("mousedown", e =>
+	{
+		mx = e.clientX;
+		my = e.clientY;
+		md = e.buttons & 1;
+		mr = (e.buttons & 2) >> 1;
+		e.preventDefault();
+		return false;
+	});
+	canvas.addEventListener("mouseup", e =>
+	{
+		mx = e.clientX;
+		my = e.clientY;
+		md = e.buttons & 1;
+		mr = (e.buttons & 2) >> 1;
+		e.preventDefault();
+		return false;
+	});
+	canvas.addEventListener("mousemove", e => { mx = e.clientX; my = e.clientY; });
+
+	let ctx = canvas.getContext("2d");
+	let w = canvas.width;
+	let h = canvas.height;
+
+	let p = [];
+	let s = [];
+
+	let px = 10, py = 10;
+
+	for (let i = 0; i < py; i++)
+		for (let j = 0; j < px; j++)
+		{
+			let x = (j-i*0.5) * (200 / (px-1)) + 200;
+			let y = i * (200 / (py-1)) * Math.sqrt(.75) + 200;
+			p.push(new Ball(x, y, 1, 0.03 / iters));
+		}
+
+	for (let i = 0; i < py; i++)
+		for (let j = 0; j < px-1; j++)
+			s.push(new Spring(p[i*px+j], p[i*px+j+1], 128.0));
+	for (let i = 0; i < py-1; i++)
+		for (let j = 0; j < px; j++)
+			s.push(new Spring(p[i*px+j], p[i*px+j+px], 128.0));
+	for (let i = 0; i < py-1; i++)
+		for (let j = 0; j < px-1; j++)
+			s.push(new Spring(p[i*px+j], p[i*px+j+px+1], 128.0));
+
+	let img_data = ctx.createImageData(w, h);
+	let img_buf = new Array(w*h);
+
+	(function loop()
+	{
+		requestAnimationFrame(loop);
+
+		ctx.fillStyle = "#000";
+		ctx.fillRect(0, 0, w, h);
+
+		ctx.fillStyle = "#FFF";
+		for (let i = 0; i < iters; i++)
+		{
+			for (let q of p)
+			{
+				let f = 0.1;
+				q.apply_force(0, 8);
+				q.apply_force(-q.vx * f, -q.vy * f);
+			}
+			for (let q of s)
+				q.apply_force();
+			for (let q of p)
+				q.update();
+			for (let q of p)
+				q.constrain();
+		}
+		for (let i = 0; i < w*h; i++)
+			img_buf[i] = 0;
+		/*
+		for (let q of p)
+		{
+			let px = Math.floor(q.x - 0.5);
+			let py = Math.floor(q.y - 0.5);
+			let fx = q.x - 0.5 - px;
+			let fy = q.y - 0.5 - py;
+
+			if (py >= 0 && py < h)
+			{
+				if (px >= 0 && px < w)
+					img_buf[py*w+px] += (1-fx)*(1-fy);
+				if (px+1 >= 0 && px+1 < w)
+					img_buf[py*w+px+1] += fx*(1-fy);
+			}
+			if (py+1 >= 0 && py+1 < h)
+			{
+				if (px >= 0 && px < w)
+					img_buf[py*w+px+w] += (1-fx)*fy;
+				if (px+1 >= 0 && px+1 < w)
+					img_buf[py*w+px+w+1] += fx*fy;
+			}
+		}
+		for (let i = 0; i < w*h; i++)
+		{
+			let a = Math.sqrt(img_buf[i]) * 255;
+			img_data.data[i*4] = a;
+			img_data.data[i*4+1] = a;
+			img_data.data[i*4+2] = a;
+			img_data.data[i*4+3] = 255;
+		}
+		ctx.putImageData(img_data, 0, 0);
+		*/
+		ctx.strokeStyle = "#FFF";
+		ctx.beginPath();
+		for (let q of s)
+		{
+			ctx.moveTo(q.b1.x, q.b1.y);
+			ctx.lineTo(q.b2.x, q.b2.y);
+		}
+		ctx.stroke();
+		for (let q of p)
+			ctx.fillRect(q.x-0.5, q.y-0.5, 1, 1);
+
+	})();
+}
+
+main();
---
+++ physics.js
@@ -0,0 +1,75 @@
+class Ball
+{
+	constructor(x, y, m, dt)
+	{
+		this.x = x;
+		this.y = y;
+		this.vx = 0;
+		this.vy = 0;
+		this.a1x = 0; // current accel 
+		this.a1y = 0;
+		this.a2x = 0; // prev accel
+		this.a2y = 0;
+		this.a3x = 0; // prev prev accel
+		this.a3y = 0;
+		this.m = m;
+		this.im = 1.0 / m;
+		this.dt = dt
+	}
+
+	apply_force(fx, fy)
+	{
+		this.a1x += fx * this.im;
+		this.a1y += fy * this.im;
+	}
+
+	update()
+	{
+		let a1x = this.a1x;
+		let a1y = this.a1y;
+		let a2x = this.a2x;
+		let a2y = this.a2y;
+		let a3x = this.a3x;
+		let a3y = this.a3y;
+		let dt = this.dt;
+		this.x += this.vx * dt + a1x * dt * dt * 0.5; // wtf why did the math work out like this
+		this.y += this.vy * dt + a1y * dt * dt * 0.5;
+		this.vx += (1.75 * a1x - a2x + 0.75 * a3x) * dt;
+		this.vy += (1.75 * a1y - a2y + 0.75 * a3y) * dt;
+		this.a3x = a2x;
+		this.a3y = a2y;
+		this.a2x = a1x;
+		this.a2y = a1y;
+		this.a1x = 0.0;
+		this.a1y = 0.0;
+	}
+	constrain()
+	{
+		if (this.y > 600)
+		{
+			this.y = 600;
+			this.vy = 0;
+		}
+	}
+}
+
+class Spring
+{
+	constructor(b1, b2, s)
+	{
+		this.b1 = b1;
+		this.b2 = b2;
+		this.l = Math.hypot(b2.x - b1.x, b2.y - b1.y);
+		this.s = s;
+	}
+	apply_force()
+	{
+		let cl = Math.hypot(this.b2.x - this.b1.x, this.b2.y - this.b1.y);
+		let dl = cl - this.l;
+		let il = 1.0 / cl;
+		let nx = (this.b2.x - this.b1.x) * il;
+		let ny = (this.b2.y - this.b1.y) * il;
+		this.b1.apply_force(nx * this.s * dl, ny * this.s * dl);
+		this.b2.apply_force(nx * this.s * -dl, ny * this.s * -dl);
+	}
+}
