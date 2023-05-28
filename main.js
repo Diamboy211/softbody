c185704a5949471fa3b6b72e1efcd527a6a11a9d
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2023-05-28 23:16:56 +0700

    every physics simulation project ends at collision detection. mostly due to frustration

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 function main()
 {
-	const iters = 256;
+	const iters = 128;
+	const frame_time = 0.03;
 	let canvas = document.getElementById("c");
+
+	let ctx = canvas.getContext("2d", { willReadFrequently: true });
+	let w = canvas.width;
+	let h = canvas.height;
+
+	let o = [];
+
+	for (let g = 0; g < 16; g++)
+	{
+		const v = 10;
+		let b = new Physics_Inflated_Object();
+		for (let i = 0; i < v; i++)
+			b.add_point(new Point(100 + (g%4)*100 + 10 * Math.cos(i/v*2*Math.PI+g), 100 + g*25 + 10 * Math.sin(i/v*2*Math.PI+g), frame_time / iters));
+		for (let h of [1, 2])
+			for (let i = 0; i < v; i++)
+				b.add_spring(b.p[i], b.p[(i+h)%v], 128.0);
+		b.update_cache();
+		b.gas = 4000;
+		o.push(b);
+	}
+
+	let img_data = ctx.createImageData(w, h);
+	let img_buf = new Array(w*h);
+
 	let mdx = 0;
 	let mdy = 0;
 	let mmx = 0;
@@ -27,40 +52,6 @@ function main()
 		return false;
 	});
 	canvas.addEventListener("mousemove", e => { mmx = e.clientX; mmy = e.clientY; });
-
-	let ctx = canvas.getContext("2d");
-	let w = canvas.width;
-	let h = canvas.height;
-
-	let p = [];
-	let s = [];
-
-	let px = 10, py = 10;
-
-	for (let i = 0; i < py; i++)
-		for (let j = 0; j < px; j++)
-		{
-			let x = (j+i) * (200 / (px-1)) + 200;
-			let y = (j-i) * (200 / (py-1)) + 200;
-			p.push(new Ball(x, y, 1, 0.03 / iters));
-		}
-
-	for (let i = 0; i < py; i++)
-		for (let j = 0; j < px-1; j++)
-			s.push(new Spring(p[i*px+j], p[i*px+j+1], 128.0));
-	for (let i = 0; i < py-1; i++)
-		for (let j = 0; j < px; j++)
-			s.push(new Spring(p[i*px+j], p[i*px+j+px], 128.0));
-	for (let i = 0; i < py-1; i++)
-		for (let j = 0; j < px-1; j++)
-		{
-			s.push(new Spring(p[i*px+j], p[i*px+j+px+1], 128.0));
-			s.push(new Spring(p[i*px+j+1], p[i*px+j+px], 128.0));
-		}
-
-	let img_data = ctx.createImageData(w, h);
-	let img_buf = new Array(w*h);
-
 	(function loop()
 	{
 		requestAnimationFrame(loop);
@@ -71,65 +62,48 @@ function main()
 		ctx.fillStyle = "#FFF";
 		for (let i = 0; i < iters; i++)
 		{
-			for (let q of p)
-			{
-				let f = 0.1;
-				q.apply_force(0, 16*q.m);
-				q.apply_force(-q.vx * f, -q.vy * f);
-			}
-			for (let q of s)
-				q.apply_force();
-			for (let q of p)
-				q.update();
-			for (let q of p)
-				q.constrain();
+			for (let a of o)
+				a.apply_force(0, 16*a.m);
+			if (md) o[0].apply_force(o[0].m*(mmx-o[0].cx), o[0].m*(mmy-o[0].cy));
+			if (mr) o[1].apply_force(o[1].m*(mmx-o[1].cx), o[1].m*(mmy-o[1].cy));
+			const f = 0.3;
+			for (let a of o)
+				for (let p of a.p)
+					p.apply_force(-p.vx * f, -p.vy * f);
+			for (let a of o)
+				a.update_force();
+			for (let a of o)
+				a.update();
+			for (let a of o)
+				for (let p of a.p)
+					p.constrain();
+			for (let i = 0; i < o.length; i++)
+				for (let j = 0; j < o.length; j++)
+					if (i != j) o[i].collide(o[j]);
 		}
-		/*
-		for (let i = 0; i < w*h; i++)
-			img_buf[i] = 0;
-		for (let q of p)
-		{
-			let px = Math.floor(q.x - 0.5);
-			let py = Math.floor(q.y - 0.5);
-			let fx = q.x - 0.5 - px;
-			let fy = q.y - 0.5 - py;
 
-			if (py >= 0 && py < h)
-			{
-				if (px >= 0 && px < w)
-					img_buf[py*w+px] += (1-fx)*(1-fy);
-				if (px+1 >= 0 && px+1 < w)
-					img_buf[py*w+px+1] += fx*(1-fy);
-			}
-			if (py+1 >= 0 && py+1 < h)
-			{
-				if (px >= 0 && px < w)
-					img_buf[py*w+px+w] += (1-fx)*fy;
-				if (px+1 >= 0 && px+1 < w)
-					img_buf[py*w+px+w+1] += fx*fy;
-			}
-		}
-		for (let i = 0; i < w*h; i++)
-		{
-			let a = Math.sqrt(img_buf[i]) * 255;
-			img_data.data[i*4] = a;
-			img_data.data[i*4+1] = a;
-			img_data.data[i*4+2] = a;
-			img_data.data[i*4+3] = 255;
-		}
-		ctx.putImageData(img_data, 0, 0);
-		*/
 		ctx.strokeStyle = "#FFF";
 		ctx.beginPath();
-		for (let q of s)
+		for (let a of o)
 		{
-			ctx.moveTo(q.b1.x, q.b1.y);
-			ctx.lineTo(q.b2.x, q.b2.y);
+			if (a instanceof Physics_Inflated_Object)
+			{
+				let j = a.p.length - 1;
+				ctx.moveTo(a.p[j].x, a.p[j].y);
+				for (let i = 0; i < a.p.length; i++)
+				{
+					ctx.lineTo(a.p[i].x, a.p[i].y);
+					j = i;
+				}
+			}
 		}
 		ctx.stroke();
-		for (let q of p)
-			ctx.fillRect(q.x-0.5, q.y-0.5, 1, 1);
-
+		for (let a of o)
+			for (let p of a.p)
+				ctx.fillRect(p.x-1, p.y-1, 2, 2);
+		
+		let img_data = ctx.getImageData(0, 0, w, h);
+		ctx.putImageData(img_data, 0, 0);
 	})();
 }
 
--- physics.js
+++ physics.js
@@ -1,6 +1,6 @@
-class Ball
+class Point
 {
-	constructor(x, y, m, dt)
+	constructor(x, y, dt)
 	{
 		this.x = x;
 		this.y = y;
@@ -12,15 +12,13 @@ class Ball
 		this.a2y = 0;
 		this.a3x = 0; // prev prev accel
 		this.a3y = 0;
-		this.m = m;
-		this.im = 1.0 / m;
 		this.dt = dt;
 	}
 
 	apply_force(fx, fy)
 	{
-		this.a1x += fx * this.im;
-		this.a1y += fy * this.im;
+		this.a1x += fx;
+		this.a1y += fy;
 	}
 
 	update()
@@ -48,22 +46,8 @@ class Ball
 		if (this.y > 600)
 		{
 			this.y = 600;
-			this.vy = 0;
-		}
-		if (this.y < 0)
-		{
-			this.y = 0;
-			this.vy = 0;
-		}
-		if (this.x > 600)
-		{
-			this.x = 600;
-			this.vx = 0;
-		}
-		if (this.x < 0)
-		{
-			this.x = 0;
-			this.vx = 0;
+			this.vx = 0.0;
+			this.vy = 0.0;
 		}
 	}
 }
@@ -77,7 +61,7 @@ class Spring
 		this.l = Math.hypot(b2.x - b1.x, b2.y - b1.y);
 		this.s = s;
 	}
-	apply_force()
+	update_force()
 	{
 		let cl = Math.hypot(this.b2.x - this.b1.x, this.b2.y - this.b1.y);
 		let dl = cl - this.l;
@@ -86,5 +70,200 @@ class Spring
 		let ny = (this.b2.y - this.b1.y) * il;
 		this.b1.apply_force(nx * this.s * dl, ny * this.s * dl);
 		this.b2.apply_force(nx * this.s * -dl, ny * this.s * -dl);
+	}
+}
+
+class Physics_Object
+{
+	constructor()
+	{
+		this.p = [];
+		this.s = [];
+
+		this.im = 1.0;
+		this.m = 1.0;
+
+		this.ax = 0.0;
+		this.ay = 0.0;
+
+	}
+	add_point(p)
+	{
+		this.p.push(p);
+	}
+	add_spring(p1, p2, s)
+	{
+		this.s.push(new Spring(p1, p2, s));
+	}
+	update_cache() { this.im = 1.0 / this.p.length; this.m = this.p.length; }
+	apply_force(x, y)
+	{
+		this.ax += x;
+		this.ay += y;
+	}
+	update_force()
+	{
+		for (let p of this.p)
+			p.apply_force(this.ax * this.im, this.ay * this.im);
+		this.ax = 0.0;
+		this.ay = 0.0;
+		for (let s of this.s)
+			s.update_force();
+	}
+	update()
+	{
+		for (let p of this.p)
+			p.update();
+	}
+	get rect()
+	{
+		let a = this.p.reduce((a, c) => Math.min(a, c.x), Infinity);
+		let b = this.p.reduce((a, c) => Math.max(a, c.x), -Infinity);
+		let c = this.p.reduce((a, c) => Math.min(a, c.y), Infinity);
+		let d = this.p.reduce((a, c) => Math.max(a, c.y), -Infinity);
+		return { x: a, y: c, w: b - a, h: d - c };
+	}
+	contains(x, y)
+	{
+		let c = 0;
+		let j = this.p.length - 1;
+		for (let i = 0; i < this.p.length; i++)
+		{
+			let p1 = this.p[j];
+			let p2 = this.p[i];
+			let x1 = p1.x - x;
+			let x2 = p2.x - x;
+			let y1 = p1.y - y;
+			let y2 = p2.y - y;
+			if (y1 != 0.0 || y2 != 0.0)
+			{
+				if (y1 * y2 <= 0.0)
+				{
+					let t = y1 / (y1 - y2);
+					let xi = x1 + (x2 - x1) * t;
+					if (xi >= 0.0) c++;
+				}
+			}
+			j = i;
+		}
+		return c % 2 == 1;
+	}
+	collide(o)
+	{
+		let r1 = this.rect;
+		let r2 = o.rect;
+		if (
+			r1.x + r1.w <= r2.x
+			&& r1.y + r1.h <= r2.y
+			&& r1.x >= r2.x + r2.w
+			&& r1.y >= r2.y + r2.h
+		) return;
+		let p = o.p.filter(a => this.contains(a.x, a.y));
+		let q = p.map(a => {
+			let j = this.p.length - 1;
+			let k1 = j, k2 = 0, md = Infinity, mt = 0.5;
+			for (let i = 0; i < this.p.length; i++)
+			{
+				let p1 = this.p[j];
+				let p2 = this.p[i];
+				let r1x = a.x - p1.x;
+				let r1y = a.y - p1.y;
+				let r2x = p2.x - p1.x;
+				let r2y = p2.y - p1.y;
+				let t = (r1x*r2x + r1y*r2y) / (r2x*r2x + r2y*r2y);
+				t = Math.max(0, Math.min(1, t));
+				let px = r2x * t;
+				let py = r2y * t;
+				let d = Math.hypot(px - r1x, py - r1y);
+				if (md > d)
+				{
+					k1 = j;
+					k2 = i;
+					md = d;
+					mt = t;
+				}
+				j = i;
+			}
+			return { a: this.p[k1], b: this.p[k2], d: md, t: mt };
+		});
+		for (let i = 0; i < p.length; i++)
+		{
+			let a = p[i];
+			let b = q[i];
+			let nx = b.b.y - b.a.y;
+			let ny = b.a.x - b.b.x;
+			let d = Math.hypot(nx, ny);
+			nx /= d;
+			ny /= d;
+			a.x += nx * d * 0.1;
+			a.y += ny * d * 0.1;
+			b.a.x += nx * d * -0.1 * (1 - b.t);
+			b.a.y += ny * d * -0.1 * (1 - b.t);
+			b.b.x += nx * d * -0.1 * b.t;
+			b.b.y += ny * d * -0.1 * b.t;
+			
+			let vax = a.vx;
+			let vay = a.vy;
+			let vbx = (b.a.vx + b.b.vx) * 0.5;
+			let vby = (b.a.vy + b.b.vy) * 0.5;
+
+			let at = vax * nx + vay * ny;
+			let bt = vbx * nx + vby * ny;
+
+			a.vx -= nx * at;
+			a.vy -= ny * at;
+
+			b.a.vx -= nx * bt;
+			b.a.vy -= ny * bt;
+			b.b.vx -= nx * bt;
+			b.b.vy -= ny * bt;
+		}
+	}
+	get cx() { return this.p.reduce((a, c) => a + c.x, 0) / this.p.length; }
+	get cy() { return this.p.reduce((a, c) => a + c.y, 0) / this.p.length; }
+}
+
+class Physics_Inflated_Object extends Physics_Object
+{
+	constructor()
+	{
+		super();
+		this.gas = 0.0;
+	}
+	update_force()
+	{
+		for (let p of this.p)
+			p.apply_force(this.ax * this.im, this.ay * this.im);
+		this.ax = 0.0;
+		this.ay = 0.0;
+		let v = this.volume;
+		let d = this.gas * this.gas / v - this.gas;
+		const inflate_strength = 0.005;
+		let j = this.p.length - 1;
+		for (let i = 0; i < this.p.length; i++)
+		{
+			let p1 = this.p[j];
+			let p2 = this.p[i];
+			let nx = p2.y - p1.y;
+			let ny = p1.x - p2.x;
+			p1.apply_force(nx * d * inflate_strength, ny * d * inflate_strength);
+			p2.apply_force(nx * d * inflate_strength, ny * d * inflate_strength);
+			j = i;
+		}
+		for (let s of this.s)
+			s.update_force();
+	}
+	get volume()
+	{
+		if (this.p.length < 3) return 0.0;
+		let v = 0.0;
+		let l = this.p.length - 1;
+		for (let i = 1; i < this.p.length - 2; i++)
+		{
+			v += (this.p[i].x - this.p[i+2].x) * (this.p[i+1].y - this.p[0].y);
+		}
+		v -= (this.p[2].x - this.p[0].x) * (this.p[1].y - this.p[0].y);
+		v += (this.p[l-1].x - this.p[0].x) * (this.p[l].y - this.p[0].y);
+		return v * 0.5;
 	}
 }
